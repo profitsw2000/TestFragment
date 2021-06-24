4b01ff46f0219b77f10bacf4cfaa9fdf83ac5319
profitsw2000 <profitsw2000@gmail.com>
2021-06-24 22:01:23 +0300

    Добавлен фрагмент динамическим способом. (FirstFragment)

--- app/src/main/java/ru/profitsw2000/testfragment/MainActivity.java
+++ app/src/main/java/ru/profitsw2000/testfragment/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.profitsw2000.testfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addFragment();
+    }
+
+    private void addFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager()   ;
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction()    ;
+        FirstFragment firstFragment = new FirstFragment()   ;
+        fragmentTransaction.add(R.id.first_fragment, firstFragment) ;
+        fragmentTransaction.commit()    ;
     }
 }
