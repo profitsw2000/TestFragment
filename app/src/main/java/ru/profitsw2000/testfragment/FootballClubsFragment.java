49acf83f01bea2b89446dcc302a87915bd210627
profitsw2000 <profitsw2000@gmail.com>
2021-11-01 21:31:10 +0300

    Set fragments to the application - one to list array of FC name, second for description of chosen club. At the start of app run first fragment with list of clubs.

---
+++ app/src/main/java/ru/profitsw2000/testfragment/FootballClubsFragment.java
@@ -0,0 +1,50 @@
+package ru.profitsw2000.testfragment;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class FootballClubsFragment extends Fragment {
+
+    View rootView   ;
+    ListView fcListView ;
+    ArrayAdapter<String> fcNamesAdapter ;
+    Context context ;
+    String[] clubs  ;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_football_clubs, container, false);
+        initUI();
+        // Inflate the layout for this fragment
+        return rootView ;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    private void initUI() {
+        context = getContext()  ;
+        clubs = getResources().getStringArray(R.array.footbal_clubs)    ;
+        fcListView = (ListView)rootView.findViewById(R.id.fc_list)  ;
+
+        fcNamesAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, clubs)    ;
+        fcListView.setAdapter(fcNamesAdapter);
+    }
+}
--- app/src/main/java/ru/profitsw2000/testfragment/MainActivity.java
+++ app/src/main/java/ru/profitsw2000/testfragment/MainActivity.java
@@ -1,14 +1,37 @@
 package ru.profitsw2000.testfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager ;
+    FragmentTransaction fragmentTransaction ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager()   ;
+        addFootballClubsFragment();
+    }
+
+    private void addFootballClubsFragment() {
+        fragmentTransaction = fragmentManager.beginTransaction();
+        FootballClubsFragment footballClubsFragment = new FootballClubsFragment()   ;
+        fragmentTransaction.add(R.id.fragment_container, footballClubsFragment) ;
+        fragmentTransaction.commit()    ;
+    }
+
+    private void addFCDescriptionFragment() {
+        fragmentTransaction = fragmentManager.beginTransaction();
+        FCDescriptionFragment footballClubsFragment = new FCDescriptionFragment()   ;
+        fragmentTransaction.replace(R.id.fragment_container, footballClubsFragment) ;
+        fragmentTransaction.addToBackStack(null)   ;
+        fragmentTransaction.commit()    ;
     }
 }
